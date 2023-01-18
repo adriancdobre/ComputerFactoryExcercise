3ede27d5eb522648868d57c3efd663190f86cc1c
adriancdobre <adydobre@gmail.com>
2023-01-18 17:38:47 +0200

    computer factory excercise

---
+++ src/Product.java
@@ -0,0 +1,75 @@
+public class Product {
+
+    private String model;
+    private String manufacturer;
+    private int width;
+    private int height;
+    private int depth;
+
+    public Product(String model, String manufacturer) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+    }
+}
+
+class Monitor extends Product {
+
+    private int size;
+
+    private String resolution;
+    public Monitor(String model, String manufacturer) {
+        super(model, manufacturer);
+    }
+
+    public Monitor(String model, String manufacturer, int size, String resolution) {
+        super(model, manufacturer);
+        this.size = size;
+        this.resolution = resolution;
+    }
+    public void drawPixelAt(int x, int y, String color){
+        System.out.println(String.format(
+                "Drawing pixel at %d,%d in color %s " ,x,y,color));
+    }
+}
+
+class Motherboard extends Product {
+
+
+    private int ramSlots;
+    private int cardSlots;
+    private int bios;
+    public Motherboard(String model, String manufacturer) {
+        super(model, manufacturer);
+    }
+
+    public Motherboard(String model, String manufacturer, int ramSlots, int cardSlots, int bios) {
+        super(model, manufacturer);
+        this.ramSlots = ramSlots;
+        this.cardSlots = cardSlots;
+        this.bios = bios;
+    }
+    public void loadProgram(String programName){
+        System.out.println("Program" + programName + "is now loading...."  );
+    }
+
+}
+
+class ComputerCase extends Product {
+
+    private String poweSupply;
+
+    public ComputerCase(String model, String manufacturer) {
+        super(model, manufacturer);
+    }
+
+    public ComputerCase(String model, String manufacturer, String poweSupply) {
+        super(model, manufacturer);
+        this.poweSupply = poweSupply;
+    }
+
+    public void pressPowerButton(){
+        System.out.println("power button pressed");
+    }
+}
+
+
